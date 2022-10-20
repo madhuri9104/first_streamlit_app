46da7104d55a3088f179cb89cbe1bc9bd3e5337b
Madhuri <115588905+madhuri9104@users.noreply.github.com>
2022-10-19 23:23:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,22 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+#create the repeatable code block(function)
+def get_fruityvice_data(this_fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+ #streamlit.text(fruityvice_response.json()) # just writes the data to the screen.
+    #take the json version of response and normalize it.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # O/P it the screen as a table.
+   return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")#New section to display fruityvice API response.
 try:
   fruit_choice=streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("please select fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    #streamlit.text(fruityvice_response.json()) # just writes the data to the screen.
-    #take the json version of response and normalize it.
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # O/P it the screen as a table.
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 #dont run anything past here while troubleshoot.
